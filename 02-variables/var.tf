73f3db6ae257d39df375eeedb5c7d29f236ec296
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-01 20:23:31 -0600

    Updating

--- 02-variables/var.tf
+++ 02-variables/var.tf
@@ -51,3 +51,9 @@ variable "sample7" {}
 output "sample7" {
   value = var.sample7
 }
+
+## Pick a variable from SHELL ENV VARIABLE
+variable "sample8" {}
+output "sample8" {
+  value = var.sample8
+}
