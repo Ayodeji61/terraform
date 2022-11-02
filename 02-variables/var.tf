c20e434a5b5d99b8b7aedc1850b274930b6cfc08
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-01 19:49:23 -0600

    Updating

--- 02-variables/var.tf
+++ 02-variables/var.tf
@@ -10,3 +10,26 @@ output "sample" {
 output "sample1" {
   value = "Hey, ${var.sample}"
 }
+
+# List Variable
+variable "sample4" {
+  default = [
+  "Hello",
+    100,
+    true
+  ]
+}
+
+# Map Variable
+variable "sample5" {
+  default = {
+    string  = "Hello",
+    number  = 100,
+    boolean = true
+  }
+}
+
+## Access a list value
+output "sample4" {
+  value = var.sample4
+}
