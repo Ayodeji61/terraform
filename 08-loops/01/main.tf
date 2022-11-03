8251a2c406d4d6849b802722365649563d1e7a2c
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-03 17:12:42 -0600

    Updating

--- 08-loops/01/main.tf
+++ 08-loops/01/main.tf
@@ -2,9 +2,9 @@ resource "aws_instance" "sample" {
     count = 2
     ami           = "ami-00d48a21603b2119b"
     instance_type = "t3.micro"
-
-    root_block_device {
-        volume_size = 15
+    tags = {
+        Name = "test-${count.index}"
+    }
     }
 
 }
