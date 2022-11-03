43ba30d6755403d0f9841fb7e1c103b858ee1c6c
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-03 17:56:53 -0600

    Updating

--- 08-loops/02/main.tf
+++ 08-loops/02/main.tf
@@ -1,11 +1,11 @@
-#resource "aws_instance" "sample" {
-   # count = length(var.instances)
-   # ami           = "ami-00d48a21603b2119b"
-   # instance_type = "t3.micro"
-  #  tags = {
-  #      Name = element(var.instances, count.index )
- #   }
-#}
+resource "aws_instance" "sample" {
+    count = length(var.instances)
+    ami           = "ami-00d48a21603b2119b"
+    instance_type = "t3.micro"
+    tags = {
+       Name = element(var.instances, count.index )
+   }
+}
 
 
 
