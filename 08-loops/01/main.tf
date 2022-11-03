2288bd2bc094f2c1247d53d2fdc6940894ae85d4
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-03 17:08:26 -0600

    Updating

---
+++ 08-loops/01/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "sample" {
+    count = 2
+    ami           = "ami-00d48a21603b2119b"
+    instance_type = "t3.micro"
+
+    root_block_device {
+        volume_size = 15
+    }
+
+}
